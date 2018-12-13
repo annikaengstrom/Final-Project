afbc06ddd28f76f6c286d17ac82edfd00846f180
Annika Engstrom <annika@ixperience.co.za>
2018-12-12 20:54:40 -0500

    Scores turned into means and focused on a positive/negative y scale.

--- app.R
+++ app.R
@@ -11,62 +11,98 @@ library(shiny)
 library(tidyverse)
 library(tidytext)
 library(readr)
+library(shinythemes)
+library(plotly)
 gss1 <- readRDS("data1.rds")
 
 # Define UI for application that draws a scatterplot
-ui <- fluidPage(
-  
-  h1("Sentiments towards homosexuals of different societal niches"),
-  "The graph below allows you to choose between different variables and see which demographics of people have different views about gay rights issues.",
-  br(),
-  br(),
-  # Sidebar with a slider input for axis inputs 
-  sidebarLayout(
-    sidebarPanel(
-      
-      selectInput(inputId = "x", 
-                  label = "X-axis:",
-                  choices = c("Marital" = "MARITAL", 
-                              "Age" = "AGE", 
-                              "Sex" = "SEX", 
-                              "Income" = "INCOME"), 
-                  selected = "AGE"),
-      
-      # Select variable for y axis
-      selectInput(inputId = "y", 
-                  label = "Y-axis:",
-                  choices = c("Right to marry" = "MARHOMO", 
-                              "Right to public speach" = "SPKHOMO", 
-                              "Right to teach" = "COLHOMO", 
-                              "Books held in libraries" = "LIBHOMO"), 
-                  selected = "MARHOMO")
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(type = "tabs",
-        tabPanel(title = "Plot",
-                 plotOutput(outputId = "scatterplot")),
-        tabPanel(title = "Summary",
-                 h5("In this analysis, I have concluded that young age increases the liklihood that someone will strongly agree that homosexuals should have the right to marry. It is also worth noting that in sentiments between Strongly Agree and Strongly Disagree, the effect of age is not as visible.
-                    "))
-      )
-    )
-  )
+ui <- fluidPage(theme = shinytheme("sandstone"),
+                
+                h1("Sentiments towards homosexuals of different societal niches"),
+                h5("The graph below allows you to choose between different variables and see which demographics of people have different views about gay rights issues."),
+                  "The graph below allows you to choose between different variables and see which demographics of people have different views about gay rights issues.",
+                br(),
+                br(),
+                # Sidebar with a slider input for axis inputs 
+                sidebarLayout(
+                  sidebarPanel(
+                    
+                    selectInput(inputId = "x", 
+                                label = "X-axis:",
+                                choices = c("Marital" = "MARITAL", 
+                                            "Age" = "AGE", 
+                                            "Sex" = "SEX", 
+                                            "Income" = "INCOME",
+                                            "Year" = "YEAR"), 
+                                  "Income" = "INCOME"), 
+                    selected = "AGE"),
+                  
+                  # Select variable for y axis
+                  selectInput(inputId = "y", 
+                              label = "Y-axis:",
+                              choices = c("Right to marry" = "MARHOMO", 
+                                          "Right to public speach" = "SPKHOMO", 
+                                          "Right to teach" = "COLHOMO", 
+                                          "Books held in libraries" = "LIBHOMO"), 
+                              selected = "MARHOMO")
+                ),
+                
+                # Show a plot of the generated distribution
+                mainPanel(
+                  tabsetPanel(type = "tabs",
+                              tabPanel(title = "Plot",
+                                       plotOutput(outputId = "barplot")),
+                                plotOutput(outputId = "scatterplot")),
+                  tabPanel(title = "Summary",
+                           h5("In this analysis, I have concluded that young age increases the liklihood that someone will strongly agree that homosexuals should have the right to marry. It is also worth noting that in sentiments between Strongly Agree and Strongly Disagree, the effect of age is not as visible.
+                              "))
+                           )
+                )
+)
 )
 
 # Define server logic required to draw a scatterplot
 server <- function(input, output) {
   
-  output$scatterplot <- renderPlot({
-      read_rds("data1.rds") %>% 
-      ggplot(aes_string(x = input$x, y = input$y)) + 
-      geom_jitter(alpha = 0.1) +
-      xlab("Demographic") +
-      ylab("Factor") +
-      ggtitle("Homosexuality Sentiment Scores")
-  })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+  y_label <- reactive({
+    req(input$y)
+    if(input$y == "MARITAL"){
+      y_label <- "Marital Status"
+    } else if(input$y == "AGE"){
+      y_label <- "Age"
+    } else if(input$y == "SEX"){
+      y_label <- "Sex"
+    } else if(input$y == "INCOME"){
+      y_label <- "Income"
+    } else if(input$y == "YEAR"){
+      y_label <- "Year"
+    }})
+  
+  x_label <- reactive({
+    req(input$x)
+    if(input$x == "MARHOMO"){
+      x_label <- "Should homosexual marriage be legal?"
+    } else if(input$x == "SPKHOMO"){
+      x_label <- "Should homosexuals have the right to public speach?"
+    } else if(input$x == "COLHOMO"){
+      x_label <- "Should homosexuals have the right to teach a college or university?"
+    } else if(input$x == "LIBHOMO"){
+      x_label <- "Should books written by homosexual authors be removed from public libraries?"
+    }})
+  
+  output$barplot <- renderPlot({
+    ggplot(data=gss1, aes_string(x = input$x, y = input$y)) +
+      stat_summary(fun.y = "mean", geom = "bar") +
+      labs(x = x_label(), y = y_label()) +
+        output$scatterplot <- renderPlot({
+          read_rds("data1.rds") %>% 
+            ggplot(aes_string(x = input$x, y = input$y)) + 
+            geom_jitter(alpha = 0.1) +
+            xlab("Demographic") +
+            ylab("Factor") +
+          ggtitle("Homosexuality Sentiment Scores")
+        })
+  }
+  
+  # Run the application 
+  shinyApp(ui = ui, server = server)
