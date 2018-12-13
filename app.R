54fb180c68e35b7f069dbc73077dded751d7e9e4
Annika Engstrom <annika@ixperience.co.za>
2018-12-12 21:17:33 -0500

    Created mean values for sentiment scores and made a positive/negative bar chart.

--- app.R
+++ app.R
@@ -20,7 +20,6 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                 
                 h1("Sentiments towards homosexuals of different societal niches"),
                 h5("The graph below allows you to choose between different variables and see which demographics of people have different views about gay rights issues."),
-                  "The graph below allows you to choose between different variables and see which demographics of people have different views about gay rights issues.",
                 br(),
                 br(),
                 # Sidebar with a slider input for axis inputs 
@@ -34,32 +33,30 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                             "Sex" = "SEX", 
                                             "Income" = "INCOME",
                                             "Year" = "YEAR"), 
-                                  "Income" = "INCOME"), 
-                    selected = "AGE"),
+                                selected = "AGE"),
+                    
+                    # Select variable for y axis
+                    selectInput(inputId = "y", 
+                                label = "Y-axis:",
+                                choices = c("Right to marry" = "MARHOMO", 
+                                            "Right to public speach" = "SPKHOMO", 
+                                            "Right to teach" = "COLHOMO", 
+                                            "Books held in libraries" = "LIBHOMO"), 
+                                selected = "MARHOMO")
+                  ),
                   
-                  # Select variable for y axis
-                  selectInput(inputId = "y", 
-                              label = "Y-axis:",
-                              choices = c("Right to marry" = "MARHOMO", 
-                                          "Right to public speach" = "SPKHOMO", 
-                                          "Right to teach" = "COLHOMO", 
-                                          "Books held in libraries" = "LIBHOMO"), 
-                              selected = "MARHOMO")
-                ),
-                
-                # Show a plot of the generated distribution
-                mainPanel(
-                  tabsetPanel(type = "tabs",
-                              tabPanel(title = "Plot",
-                                       plotOutput(outputId = "barplot")),
-                                plotOutput(outputId = "scatterplot")),
-                  tabPanel(title = "Summary",
-                           h5("In this analysis, I have concluded that young age increases the liklihood that someone will strongly agree that homosexuals should have the right to marry. It is also worth noting that in sentiments between Strongly Agree and Strongly Disagree, the effect of age is not as visible.
-                              "))
-                           )
+                  # Show a plot of the generated distribution
+                  mainPanel(
+                    tabsetPanel(type = "tabs",
+                                tabPanel(title = "Plot",
+                                         plotOutput(outputId = "barplot")),
+                                tabPanel(title = "Summary",
+                                         h5("In this analysis, I have concluded that young age increases the liklihood that someone will strongly agree that homosexuals should have the right to marry. It is also worth noting that in sentiments between Strongly Agree and Strongly Disagree, the effect of age is not as visible.
+                                            "))
+                                         )
+                    )
+                  )
                 )
-)
-)
 
 # Define server logic required to draw a scatterplot
 server <- function(input, output) {
@@ -94,15 +91,9 @@ server <- function(input, output) {
     ggplot(data=gss1, aes_string(x = input$x, y = input$y)) +
       stat_summary(fun.y = "mean", geom = "bar") +
       labs(x = x_label(), y = y_label()) +
-        output$scatterplot <- renderPlot({
-          read_rds("data1.rds") %>% 
-            ggplot(aes_string(x = input$x, y = input$y)) + 
-            geom_jitter(alpha = 0.1) +
-            xlab("Demographic") +
-            ylab("Factor") +
-          ggtitle("Homosexuality Sentiment Scores")
-        })
-  }
-  
-  # Run the application 
-  shinyApp(ui = ui, server = server)
+      ggtitle("Homosexuality Sentiment Scores")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
